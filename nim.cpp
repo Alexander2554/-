94dfaff1be34863824fd4d18da910ce1fbf68fe5
alexander_2554 <gtavvvvv@mail.ru>
2020-06-16 17:24:59 +0700

    create code

--- nim.cpp
+++ nim.cpp
@@ -1,36 +1,32 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
-
 int main()
 {
-    const int RODS(15);
-    const int OBJECTS(10);
+    const int KNOTS(15);
+    const int MATCHS(10);
     int * array;
-    int number_of_rods(1);
+    int number_of_knots(1);
     int player(1);
-    int rod_chosen;
-    int objects_removed;
+    int knot_chosen;
+    int matchs_removed;
     bool allzero(false);
     // Алгоритм:
     // Подсказка и считывание количества кучек
-    number_of_rods=read_rods (RODS);
+    number_of_knots=read_knots (KNOTS);
     // Подсказка и считывание количества спичек в каждой кучки
-    array=new int [number_of_rods];
-    procedure (array,number_of_rods,OBJECTS);
+    array=new int [number_of_knots];
+    procedure (array,number_of_knots,MATCHS);
     // Нарисуйте спички в кучке
-    draw_rods (array,number_of_rods);
+    draw_knots (array,number_of_knots);
     // Отображение статистики
-    statistics (array,number_of_rods);
+    statistics (array,number_of_knots);
     // Пока какая-то кучка не бюудет пуста
     while (!allzero)
     {
         // Подсказать и прочитать следующий ход игрока
-        next_move (array,number_of_rods,player,rod_chosen,objects_removed);
+        next_move (array,number_of_knots,player,knot_chosen,matchs_removed);
         // Удалить указанное количество спичек из указанной кучки
-        move_objects (array,rod_chosen,objects_removed);
+        move_matchs (array,knot_chosen,matchs_removed);
         // Если все кучи пусты, то
-        allzero=all_zero (array,number_of_rods);
+        allzero=all_zero (array,number_of_knots);
         if(allzero)
         {
             // Распечатать сообщение, поздравляющее победившего игрока.
@@ -40,9 +36,9 @@ int main()
         else
         {
             // Перерисуйте кучки с процентами
-            draw_rods (array,number_of_rods);
+            draw_knots (array,number_of_knots);
             // Отображение статистики
-            statistics (array,number_of_rods);
+            statistics (array,number_of_knots);
             // Переключиться на другого игрока
             switch_the_turn (player);
             // КОНЕЦ, ЕСЛИ
@@ -52,4 +48,3 @@ int main()
     delete [] array;
     return 0;
 }
-
