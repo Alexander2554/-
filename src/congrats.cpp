e40fef830be9280be2d2f3306268bf6d65a3bb93
alexander_2554 <gtavvvvv@mail.ru>
2020-06-20 16:36:46 +0700

    add congrats

---
+++ src/congrats.cpp
@@ -0,0 +1,21 @@
+#include <iomanip>
+#include <iostream>
+using namespace std; 
+
+ void congrats(int player)
+    {
+        cout << endl;
+        cout << "Поздравляю! Игрок " << player << " выиграл." << endl;
+    }
+
+    /*
+    Эта функция переключает поворот на другого игрока.
+    */
+    void switch_the_turn(int& player)
+    {
+        if (player == 1) {
+            player = 2;
+        } else {
+            player = 1;
+        }
+    }
---
+++ src/congrats.hpp
@@ -0,0 +1,7 @@
+#ifndef CONGRATS_H
+#define CONGRATS_H
+using namespace std;
+
+void congrats(int player);
+void switch_the_turn(int& player);
+#endif
